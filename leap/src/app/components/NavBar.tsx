e0878c165c6d165d0729fdf79338c31dd18699de
alexkings47 <michealexcel54@gmail.com>
2024-10-01 18:01:52 +0100

    project init

---
+++ leap/src/app/components/NavBar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div>
+        <div className="div"><div></div>
+        <div><img src='' alt='logo' className='' />
+        
+        </div></div>
+        <div className="div"></div>
+    </div>
+  )
+}
+
+export default NavBar
